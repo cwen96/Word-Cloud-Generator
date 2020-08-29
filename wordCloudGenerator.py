6d59322734cbbf667e2b70539c699496b5d3dc11
Chris Wen <chriswen96@gmail.com>
2020-08-29 01:28:43 -0700

    Added GUI

--- wordCloudGenerator.py
+++ wordCloudGenerator.py
@@ -2,6 +2,20 @@
 
 import wordcloud
 from matplotlib import pyplot as plot
+from tkinter import *
+from tkinter import ttk
+from tkinter.filedialog import askopenfile
+
+def readFileAndDrawPlot():
+    file = askopenfile(mode = 'rb', filetypes = [("Text Files", "*.txt")])
+    fileContents = ""
+    for i in file:
+        fileContents += str(i, 'utf-8')
+    myimage = calculate_frequencies(fileContents)
+    plot.imshow(myimage, interpolation = 'nearest')
+    plot.axis('off')
+    plot.show()
+    
 
 #Removes punctuation from words
 def omit_punctuation(word, punctuation):
@@ -12,7 +26,7 @@ def omit_punctuation(word, punctuation):
     return newWord
 
 #Calculates how many times each word appears in the text file
-def calculate_frequencies(file_data):
+def calculate_frequencies(fileContents):
     #List of punctuations and uninteresting words to be excluded from the wordcloud
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
@@ -22,12 +36,11 @@ def calculate_frequencies(file_data):
     "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just", "for", "in", "not", "gutenbergtm", "into"]
     
     wordCount = {}
-    for word in file_data.split():
+    for word in fileContents.split():
         lowercase = word.lower()
         lowercase = omit_punctuation(lowercase, punctuations)
-        if lowercase.isalpha():
-            if lowercase in uninteresting_words:
-                continue
+        if not lowercase.isalpha() or lowercase in uninteresting_words:
+            continue
         if lowercase in wordCount:
             wordCount[lowercase] += 1
         else:
@@ -37,9 +50,19 @@ def calculate_frequencies(file_data):
     cloud.generate_from_frequencies(wordCount)
     return cloud.to_array()
 
-with open('The Project Gutenberg EBook of Mewanee.txt', encoding='utf8') as file:
-    file_data = file.read()
-    myimage = calculate_frequencies(file_data)
-    plot.imshow(myimage, interpolation = 'nearest')
-    plot.axis('off')
-    plot.show()
+def main1():
+    base = Tk()
+    base.geometry("300x100")
+    Title = base.title( "File Opener")
+    label = ttk.Label(base, text ="Please open a text file",foreground="black",font=("Calibri", 20))
+    label.pack()
+    menu = Menu(base)
+    base.config(menu=menu)
+    file = Menu(menu, tearoff = False)
+    menu.add_cascade(label = "File", menu = file)
+    file.add_command(label = "Open", command = readFileAndDrawPlot)
+    file.add_command(label = "Exit", command = exit)
+    mainloop()
+
+if __name__ == "__main__":
+    main1()
